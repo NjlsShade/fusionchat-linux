534f741d6df54e5b28b5ad6647daa9567efd3e29
NjlsShade <njls.yoshi2@gmail.com>
2017-04-17 10:04:29 +0000

    Added Windows support

---
+++ fusion shell.bat
@@ -0,0 +1,3 @@
+start binary\x11\x11.exe -silent-dup-error -ac -notrayicon -multiwindow -multimonitors
+binary\bin\bash.exe fusion -c
+taskkill /F /IM x11.exe
---
+++ fusion.bat
@@ -0,0 +1,3 @@
+start binary\x11\x11.exe -silent-dup-error -ac -notrayicon -multiwindow -multimonitors
+binary\bin\bash.exe fusion
+taskkill /F /IM x11.exe
